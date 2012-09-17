0151f0f19b455d7466e57ec8e335c8a5c023ca88
codeglomeration <mirceagrelus@gmail.com>
2012-09-18 01:43:06 +0300

    updates

--- server.rb
+++ server.rb
@@ -3,7 +3,8 @@
   include WEBrick
 
   dir = Dir::pwd
-  port = 12000 + (dir.hash % 1000)
+  #port = 12000 + (dir.hash % 1000)
+  port = 12276
 
   puts "URL: http://#{Socket.gethostname}:#{port}"
 
